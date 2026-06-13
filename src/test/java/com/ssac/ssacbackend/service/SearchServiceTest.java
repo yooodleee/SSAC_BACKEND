a3ae072029dfb4b8fed44299e45ed551eb368002
yooodleee <yooodleee@gmail.com>
2026-06-13 23:01:29 +0900

    fix: [오민수] 미사용 import를 제거합니다. [SSACBE-20]

--- src/test/java/com/ssac/ssacbackend/service/SearchServiceTest.java
+++ src/test/java/com/ssac/ssacbackend/service/SearchServiceTest.java
@@ -27,7 +27,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.util.ReflectionTestUtils;
 
