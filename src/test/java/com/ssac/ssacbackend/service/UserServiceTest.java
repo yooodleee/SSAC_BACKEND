d77e949404f2bdda9f268202b70636b83e3a2b22
yooodleee <yooodleee@gmail.com>
2026-06-19 12:25:54 +0900

    fix: [오민수] 미사용 import를 제거합니다. [SSACBE-21]

--- src/test/java/com/ssac/ssacbackend/service/UserServiceTest.java
+++ src/test/java/com/ssac/ssacbackend/service/UserServiceTest.java
@@ -3,8 +3,6 @@ package com.ssac.ssacbackend.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
